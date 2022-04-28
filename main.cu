320b41946fd4fa6676410f0581afa3006bbf53f9
aloggia <aloggia22@gmail.com>
2022-04-27 22:11:05 -0400

    Cuda dot product works. Working on cpu dot product

--- main.cu
+++ main.cu
@@ -1,13 +1,23 @@
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 #define THREADS_PER_BLOCK 256
 #define NUM_BLOCKS 256
 
-__global__ void dotp(float *u, float *v, float *partialSum, int n) {
+
+float cpudotp(float u[], float v[], int n) {
+    float partialSum = 0.0;
+    for (int i = 0; i < n; ++i) {
+        partialSum = u[i] * v[i];
+    }
+    return partialSum;
+}
+
+__global__ void dotp(const float *u, const float *v, float *partialSum, int n) {
 
     __shared__ float cache[THREADS_PER_BLOCK];
     int cacheIndex = threadIdx.x;
@@ -17,18 +27,20 @@ __global__ void dotp(float *u, float *v, float *partialSum, int n) {
     for (int i = blockIdx.x * blockDim.x + threadIdx.x; i < n; i = i + stride) {
         temp = temp + u[i] * v[i];
     }
-
     cache[cacheIndex] = temp;
     __syncthreads();
 
-    cacheIndex = threadIdx.x;
     int i = blockDim.x / 2;
     while (i > 0) {
         if (cacheIndex < i) {
             cache[cacheIndex] = cache[cacheIndex] + cache[cacheIndex + i];
             __syncthreads();
         }
-        i = i / 2;
+        i /= 2;
+    }
+
+    if (cacheIndex == 0) {
+        partialSum[blockIdx.x] = cache[cacheIndex];
     }
 }
 
@@ -40,34 +52,67 @@ __global__ void add(int *x, int *y, int *z, int n) {
 }
 
 int main() {
-    int N = 1 << 20; //Number of elements in arrays
-    float *x, *y, *d_x, *d_y, *partialSum, d_partialSum;
+    //TODO: Create a
+    srand(time(nullptr));
+    int N = 256; //Number of elements in arrays
 
-    x = (float*)malloc(N * sizeof(float));
-    y = (float*) malloc(N * sizeof(float));
+    float *h_x, *h_y, *d_x, *d_y, *h_partialSum, *d_partialSum;
 
-    cudaMalloc(&d_x, N * sizeof(float));
-    cudaMalloc(&d_y, N * sizeof(float));
+    float  cpu_x [256];
+    float cpu_y [256];
 
-    partialSum = (float*)malloc(NUM_BLOCKS * sizeof(float));
-    // TODO: Set each element to a random num between 0 and 1
+    h_x = new float [N];
+    h_y = new float [N];
+    h_partialSum = new float [THREADS_PER_BLOCK];
+
+    cudaMalloc((void **)&d_x, N * sizeof(float));
+    cudaMalloc((void **)&d_y, N * sizeof(float));
+    cudaMalloc((void **)&d_partialSum, THREADS_PER_BLOCK * sizeof(float));
+
+    //h_x = (float*)malloc(N * sizeof(float));
+    //h_y = (float*) malloc(N * sizeof(float));
+    //h_partialSum = (float*) malloc(N * sizeof(float));
+
     for (int i = 0; i < N; ++i) {
-        x[i] = (float) (i + 1);
-        y[i] = 1.0 / U[i];
+        h_x[i] = (float)rand() / (float)RAND_MAX;
+        cpu_x[i] = h_x[i];
+        h_y[i] = (float)rand() / (float)RAND_MAX;
+        cpu_y[i] = h_y[i];
     }
 
-    cudaMemcpy( d_x, x, N*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy( d_y, y, N*sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy( d_partialSum, partialSum, NUM_BLOCKS * sizeof(float), cudaMemcpyHostToDevice);
 
 
+    cudaMemcpy(d_x, h_x, N * sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, h_y, N * sizeof(float), cudaMemcpyHostToDevice);
+
+   // h_partialSum = (float*)malloc(NUM_BLOCKS * sizeof(float));
+    // TODO: Set each element to a random num between 0 and 1
+
+    cout << h_x[0] << " + " << h_y[0] << " = " << h_x[0] + h_y[0] << endl;
     dotp<<<THREADS_PER_BLOCK, NUM_BLOCKS>>>(d_x, d_y, d_partialSum, N);
     cudaDeviceSynchronize();
-    cudaMemcpy(partialSum, d_partialSum, NUM_BLOCKS*sizeof(float), cudaMemcpyHostToDevice);
+    cudaMemcpy(h_partialSum, d_partialSum, NUM_BLOCKS * sizeof(float), cudaMemcpyDeviceToHost);
+
 
     float gpuResult = 0.0;
     for (int i = 0; i < NUM_BLOCKS; ++i) {
-        gpuResult = gpuResult + partialSum[i];
+        gpuResult = gpuResult + h_partialSum[i];
+    }
+    cout << "Value calculated from the GPU: " << gpuResult << endl;
+
+    cout << "Value calculated from the CPU: " << cpudotp(cpu_x, cpu_y, N) << endl;
+
+
+    cudaFree(d_x);
+    cudaFree(d_y);
+    cudaFree(d_partialSum);
+
+    for (int i = 0; i < N; ++i) {
+
     }
 
+
+    delete [] h_x;
+    delete [] h_y;
+    delete [] h_partialSum;
 }
